0fc0611c6d69245f78a9d755efcc9f8152bdd40c
Michael Fahim <michael_fahim@yahoo.com>
2020-07-19 17:07:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,13 +9,5 @@ puts params
 @rams = params
 erb :team
 end
-post '/team' do
-  "Team Name: #{params[:name]}
-  Coach: #{params[:coach]}
-  Point Guard #{params[:pg]}
-  Shooting Guard #{params[:sg]}
-  Small Forward #{params[:sf]}
-  Power Forward #{params[:pf]}
-  Center #{params[:c]}"
-end
+
 end
